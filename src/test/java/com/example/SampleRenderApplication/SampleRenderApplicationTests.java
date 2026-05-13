c85b52ccf033c1aa5336cbb5400d2f108a99220b
Ruptam <ruptam25@outlook.com>
2026-05-13 19:14:25 +0530

    First initial commit for deployment

---
+++ src/test/java/com/example/SampleRenderApplication/SampleRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SampleRenderApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
